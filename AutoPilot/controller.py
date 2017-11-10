0270a6da6735c5782631bd35e218e1a9d876e5fb
dagan <dagan.pielstick@gmail.com>
2017-11-10 14:53:52 -0700

    Updated controller code syntatx to match example

--- AutoPilot/controller.py
+++ AutoPilot/controller.py
@@ -1,31 +1,116 @@
+#!/usr/bin/env python
+'''
+import rospy
+from rosplane_msgs.msg import State
+from rosflight_msgs.msg import GPS, Command
+'''
 import numpy as np
+class currentGPS:
+    def __init__(self):
+        self.latitude=0
+        self.longitude=0
+        self.altitude=0
+        self.speed=0
+        self.ground_course=0
+class command:
+    def __init__(self):
+        self.F=0
+        self.x=0
+        self.y=0
+        self.z=0
 
-class GPSreading:
-    def __init__(self,lat,log,heading,vel,alt):
-        self.lat=lat
-        self.log=log
-        self.heading=heading
-        self.vel=vel
-        self.alt=alt
-#Cacluate angle between position and waypoint
-def eangle(lat,log,targetx,targety):
-    angle=np.arctan2(log-targety,lat-targetx)
-    return angle
+class autopilot:
+    def __init__(self,speed,heading,alt):
+        self.currentGPS=currentGPS()
+        self.command=command()
+        self.update_rate = 20
+        self.currentGPS.latitude=40.267198
+        self.currentGPS.longitude=-111.635642# Deg
+        self.currentGPS.altitude=alt # m
+        self.currentGPS.speed=speed # m/s
+        self.currentGPS.ground_course=heading # rad clockwise from the north
 
-#Build controller here
-def controller(GPS):
-    #Controller Setup
-    #Controller Sensatvity Values
-    krudder=0.01
+        #self.state_subscriber = rospy.Subscriber("/fixedwing/state", State, self.stateCallback, queue_size=20)
+        #self.gps_subscriber = rospy.Subscriber("/fixedwing/gps", GPS, self.gpsCallback, queue_size=10)
+        #self.currentState = State()
+        #self.currentGPS = GPS()
 
-    #Target Locations
-    targetx=4
-    targety=5
-    print GPS.heading
-    #Get heading direction
-    htarget=eangle(GPS.lat,GPS.log,targetx,targety)
-    #Calculate heading error
-    error=GPS.heading-htarget
-    #proportional controller
-    rudder=krudder*error
-    return rudder
+        #rospy.Timer(rospy.Duration(1./self.update_rate), self.control)
+        #self.command_publisher = rospy.Publisher("/fixedwing/command", Command, queue_size=1)
+    def simulationStep(self,dt):
+        #Simplified Rudder Model
+        self.currentGPS.ground_course=self.currentGPS.ground_course+self.command.z
+        relVel=self.currentGPS.speed*0.07
+        lat=self.currentGPS.latitude-40.0
+        log=self.currentGPS.longitude+111.
+        gc=self.currentGPS.ground_course
+        lat=lat-relVel*np.cos(gc)*dt
+        log=log+relVel*np.sin(gc)*dt
+        #Calculate New Position
+        self.currentGPS.latitude=lat+40.0
+        self.currentGPS.longitude=log-111.0
+
+    def stateCallback(self, msg):
+        'dummy'
+        #self.currentState = msg
+
+    def gpsCallback(self, msg):
+        if msg.fix and msg.NumSat > 3:
+            'empty'
+            #self.currentGPS = msg
+
+    def control(self):#, event):
+
+        #command = Command()
+        #command.mode = command.MODE_PASS_THROUGH
+        #command.ignore = command.IGNORE_NONE
+
+        # edit starting right here!!!!
+        #Controller Sensatvity Values
+        krudder=2
+        #Target Locations
+        targetx=40.267638-40
+        targety=-111.635170+111
+        lat=self.currentGPS.latitude-40
+        log=self.currentGPS.longitude+111
+        #Calculate Target Angle
+        htarget=np.arctan2(lat-targetx,log-targety)
+        #Calculate heading error
+        error=self.currentGPS.ground_course-htarget
+        print 'error: ',error
+        rudder=-krudder*error
+        if rudder>5:
+            rudder=5
+        elif rudder<-5:
+            rudder=-5
+        rudderscale=rudder/5.0
+
+        # you can get GPS sensor information:
+        # self.currentGPS.latitude # Deg
+        # self.currentGPS.longitude # Deg
+        # self.currentGPS.altitude # m
+        # self.currentGPS.speed # m/s
+        # self.currentGPS.ground_course # rad clockwise from the north
+
+        # you can get the state estimate information:
+        # self.currentState.position[0:3] # north, east, down (m) from starting location
+        # self.currentState.Va		# Airspeed (m/s)
+        # self.currentState.phi		# Roll angle (rad)
+        # self.currentState.theta		# Pitch angle (rad)
+        # self.currentState.chi_deg		# Wrapped (-180 to 180) course angle (deg)
+        # self.currentState.p		# Body frame rollrate (rad/s)
+        # self.currentState.q		# Body frame pitchrate (rad/s)
+        # self.currentState.r		# Body frame yawrate (rad/s)
+
+        self.command.F = 0.75 # throttle command 0.0 to 1.0
+        self.command.x = 0.0 # aileron servo command -1.0 to 1.0  positive rolls to right
+        self.command.y = -0.03 # elevator servo command -1.0 to 1.0  positive pitches up
+        self.command.z = rudderscale # rudder servo command -1.0 to 1.0  positive yaws to left
+
+        #self.command_publisher.publish(command)
+'''
+if __name__ == '__main__':
+    rospy.init_node('autopilot_py', anonymous=True)
+    ap = autopilot()
+    rospy.spin()
+'''
--- AutoPilot/simulation.py
+++ AutoPilot/simulation.py
@@ -1,26 +1,19 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
-import controller
-class GPSreading:
-    def __init__(self,lat,log,heading,vel,alt):
-        self.lat=lat
-        self.log=log
-        self.heading=heading
-        self.vel=vel
-        self.alt=alt
-
+#import controller
+#import controller
+from controller import autopilot
+heading = 0
+velocity = 14
+alt=10
+AP=autopilot(velocity,heading,alt)
 #Length of simulation (Iterations)
 lSim=100
 #Set Default values
 #GPS Frequency
-gpsfreq=10
-#Starting Values
-heading=0
-Velocity=1
-lat=0
-log=0
-alt=10
+gpsfreq=1
+
 #Control Surface Starting Point
 rudder=0
 
@@ -29,36 +22,34 @@ timestep=0 #Start at 0 Time
 windspeed = 0 #To implement
 #Calculate dt
 dt=1.0/gpsfreq
-#Initalize GPS Class
-GPS = GPSreading(lat,log,heading,Velocity,alt)
+#Initalize Autopilot Class
 
-def simulationStep(dt,GPS,rudder):
-    #Simplified Rudder Model
-    GPS.heading=GPS.heading+rudder
-    #Calculate New Position
-    GPS.lat=GPS.lat+GPS.vel*np.cos(GPS.heading)*dt
-    GPS.log=GPS.log+GPS.vel*np.sin(GPS.heading)*dt
-    return GPS
-def plotter(GPS,targets,fig):
+
+def plotter(AP,targets,fig):
     plt.figure(fig)
-    plt.scatter(GPS.lat,GPS.log)
+    plt.scatter(AP.latitude,AP.longitude)
 
 #Data Save
 lat=np.zeros(lSim)
 log=np.zeros(lSim)
+hed=np.zeros(lSim)
 while lSim>timestep:
     #Call Controller
-    rudder=controller.controller(GPS)
+    AP.control()
+    #rudder=controller.controller(AP)
     #Step Simulation Forward
-    GPS=simulationStep(dt,GPS,rudder)
+    AP.simulationStep(dt)
     #Log Lat and Longitude
-    lat[timestep]=GPS.lat
-    log[timestep]=GPS.log
-
-    print GPS.lat,GPS.log
+    lat[timestep]=AP.currentGPS.latitude
+    log[timestep]=AP.currentGPS.longitude
+    hed[timestep]=AP.currentGPS.ground_course
+    print AP.currentGPS.latitude,AP.currentGPS.longitude,AP.command.z
     #Step Forward
     timestep+=1
-plt.scatter(lat,log)
-plt.scatter(4,5,color='red')
+plt.plot(lat-40,log+111)
+#Calculate heading line
+plt.plot(lat+5*np.cos(hed),log+5*np.sin(hed))
+
+plt.scatter(40.267638,-111.635170,color='red')
 plt.show()
 print 'Done'
