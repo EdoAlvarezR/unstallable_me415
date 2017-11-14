314678b8421eb56098245d34f3dc7fb6901e67fa
Dagan Pielstick <dagan.pielstick@gmail.com>
2017-11-14 06:30:09 -0700

    Added Timeaveraging and MulitTarget Accomidation

--- AutoPilot/controller.py
+++ AutoPilot/controller.py
@@ -29,7 +29,23 @@ class autopilot:
         self.currentGPS.altitude=alt # m
         self.currentGPS.speed=speed # m/s
         self.currentGPS.ground_course=heading # rad clockwise from the north
-        self.target=0
+
+        #------------------------------------------------------------------------------
+        #Target Selection
+        self.target=0 #Home/Starting Point
+        #Target Locations
+        self.targetlats=[40.267198,40.267638,40.267738,40.26752]
+        self.targetlogs=[-111.635642,-111.635170,-111.635670,-111.635870]
+        #Initialization Changes
+        self.isFirst=True
+        #Running Average Size
+        self.tsize=5
+        #Running Average Data Storage
+        self.lats=np.ones(self.tsize)
+        self.logs=np.ones(self.tsize)
+        self.ha=np.ones(self.tsize)
+        self.alt=np.ones(self.tsize)
+        #--------------------------------------------------------------------------------
         #self.state_subscriber = rospy.Subscriber("/fixedwing/state", State, self.stateCallback, queue_size=20)
         #self.gps_subscriber = rospy.Subscriber("/fixedwing/gps", GPS, self.gpsCallback, queue_size=10)
         #self.currentState = State()
@@ -39,7 +55,7 @@ class autopilot:
         #self.command_publisher = rospy.Publisher("/fixedwing/command", Command, queue_size=1)
     def simulationStep(self,dt):
         #Simplified Rudder Model
-        self.currentGPS.ground_course=self.currentGPS.ground_course-self.command.z
+        self.currentGPS.ground_course=self.currentGPS.ground_course-self.command.z/5
         #Current Vel~= 100m/s
         relVel=self.currentGPS.speed*0.0000007
         lat=self.currentGPS.latitude
@@ -72,12 +88,36 @@ class autopilot:
         #Read in Data
         lat=self.currentGPS.latitude
         log=self.currentGPS.longitude
+        head=self.currentGPS.ground_course
+        alti=self.currentGPS.altitude
 
-        #Load Target Location
-        #Dummy Target Locations
-        tlat=40.267638
-        tlog=-111.635170
-        #Test For Location
+        if self.isFirst:
+            self.isFirst=False
+            #Initalize Time Averaging at inital point
+            self.lats=self.lats*lat
+            self.logs=self.logs*log
+            self.ha=self.ha*head
+            self.alt=self.alt*alti
+        #Timeaverage Data
+        #Shift Data Backwards
+        self.lats=np.roll(self.lats,1)
+        self.logs=np.roll(self.logs,1)
+        self.ha=np.roll(self.ha,1)
+        self.alt=np.roll(self.alt,1)
+        #Rewrite oldest point
+        self.lats[0]=lat
+        self.logs[0]=log
+        self.ha[0]=head
+        self.alt[0]=alti
+        #Calculate Average Value
+        lat=np.average(self.lats)
+        log=np.average(self.logs)
+        head=np.average(self.ha)
+        alti=np.average(self.alt)
+
+        #select target location
+        tlat=self.targetlats[self.target]
+        tlog=self.targetlogs[self.target]
 
         #Calculate Distance
         radius=6371*1000.0
@@ -88,20 +128,41 @@ class autopilot:
         c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
         d = radius * c
 
-        print d
+        #Test for locations
+        if d<7:
+            self.target+=1
+            print "Waypoint Reached"
+            if self.target>3:
+                print "Going Home"
+                self.target=0
+
 
         #Calculate Target Angle
         htarget=np.arctan2(tlog-log,tlat-lat)
         #Calculate heading error
-        error=htarget-self.currentGPS.ground_course
+        error=htarget-head
+        #Debugging Code
         print 'error: ',error
         self.harget=htarget
+        #Proportional Controller
         rudder=-krudder*error
+        #Normalization of Rudder Control
         if rudder>5:
             rudder=5
         elif rudder<-5:
             rudder=-5
         rudderscale=rudder/5.0
+        #Altitue Check
+        hmin=200*0.3048
+        hmax=300*0.3048
+        #Throttles are arbitrary values
+        if hmin>alti:
+            throttle=0.8
+        elif hmax<alti:
+            throttle=0.4
+        else:
+            throttle=0.6
+
         # you can get GPS sensor information:
         # self.currentGPS.latitude # Deg
         # self.currentGPS.longitude # Deg
@@ -119,9 +180,10 @@ class autopilot:
         # self.currentState.q		# Body frame pitchrate (rad/s)
         # self.currentState.r		# Body frame yawrate (rad/s)
 
-        self.command.F = 0.75 # throttle command 0.0 to 1.0
+        self.command.F = throttle # throttle command 0.0 to 1.0
         self.command.x = 0.0 # aileron servo command -1.0 to 1.0  positive rolls to right
-        self.command.y = -0.03 # elevator servo command -1.0 to 1.0  positive pitches up
+        self.command.y = 0.0 # elevator servo command -1.0 to 1.0  positive pitches up
+
         self.command.z = rudderscale # rudder servo command -1.0 to 1.0  positive yaws to left
         #-------------------------------------------------------------------------------------------------------------
         #self.command_publisher.publish(command)
--- AutoPilot/simulation.py
+++ AutoPilot/simulation.py
@@ -9,7 +9,7 @@ velocity = 14
 alt=10
 AP=autopilot(velocity,heading,alt)
 #Length of simulation (Iterations)
-lSim=100
+lSim=200
 #Set Default values
 #GPS Frequency
 gpsfreq=1
@@ -53,15 +53,20 @@ fig1=plt.figure()
 ax1 = fig1.add_subplot(111, aspect='equal')
 plt.plot(plat,plog,label='Position')
 sc=0.0001
+targetlats=[40.267198,40.267638,40.267738,40.26752]
+targetlogs=[-111.635642,-111.635170,-111.635670,-111.635870]
+
 #Calculate heading line
-for j in range(0,len(hed)):
-    plt.plot([plat[j],plat[j]+sc*np.cos(hed[j])],[plog[j],plog[j]+sc*np.sin(hed[j])],color='red',linewidth='0.5')
-    plt.plot([plat[j],plat[j]+sc*np.sin(ht[j])],[plog[j],plog[j]+sc*np.cos(ht[j])],color='green',linewidth='0.5')
+#for j in range(0,len(hed)):
+    #plt.plot([plat[j],plat[j]+sc*np.cos(hed[j])],[plog[j],plog[j]+sc*np.sin(hed[j])],color='red',linewidth='0.5')
+    #plt.plot([plat[j],plat[j]+sc*np.sin(ht[j])],[plog[j],plog[j]+sc*np.cos(ht[j])],color='green',linewidth='0.5')
 #plt.plot(plat+5*np.sin(hed),plog+5*np.cos(hed),label='Heading')
-plt.legend()
+#plt.legend()
+plt.scatter(targetlats,targetlogs,color='r')
 plt.scatter(40.267198,-111.635642,color='k')
-plt.scatter(40.267638,-111.635170,color='red')
-circ=plt.Circle((40.267638,-111.635170),0.00012,fill=False,color='r',linestyle='--')
-ax1.add_patch(circ)
+#Estimate of 10 m radius
+for j in range(0,len(targetlats)):
+    circ=plt.Circle((targetlats[j],targetlogs[j]),0.00012,fill=False,color='r',linestyle='--')
+    ax1.add_patch(circ)
 plt.show()
 print 'Done'
